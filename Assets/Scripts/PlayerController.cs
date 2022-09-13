8b94c5542e8120d03a556cee890828871ca9331b
Christian-Placencia <chbasaldua123@gmail.com>
2022-09-13 10:11:14 -0500

    Implemented Third-Person camera and controller
    Implemented a third-person camera system using Cinemachine that collides with obstacles. Wrote a PlayerController script that moves and turns the Player according to camera movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,54 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Variables
+    // Public variables
+    [Tooltip("CharacterController component")]
+    public CharacterController controller;
+
+    [Tooltip("Camera Transform component")]
+    public Transform cam;
+
+    [Tooltip("Player's movement speed")]
     public float speed = 15.0f;
+
+    [Tooltip("Player's turn speed")]
+    public float smoothTime = 0.1f;
+    
+    [Tooltip("Horizontal input axis")]
     public string horizontalInputName = "Horizontal";
+
+    [Tooltip("Vertical input axis")]
     public string verticalInputName = "Vertical";
-    private float horizontalInput;
-    private float verticalInput;
+
+
+    // Private variables
+    float turnSmoothVelocity; // Used to smooth out the player's turning
 
     // Update is called once per frame
     void Update()
     {
         // Check user input.
-        horizontalInput = Input.GetAxis(horizontalInputName);
-        verticalInput = Input.GetAxis(verticalInputName);
+        float horizontalInput = Input.GetAxisRaw(horizontalInputName);
+        float verticalInput = Input.GetAxisRaw(verticalInputName);
 
-        // Move the player according to user input.
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        // Calculate the direction the player is moving.
+        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
-        // Lock player rotation.
-        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        // Check if the player is moving.
+        if (direction.magnitude >= 0.1f)
+        {
+            // Calculate the angle the player should be facing.
+            float facingAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+            // Smooth the angle.
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, facingAngle, ref turnSmoothVelocity, smoothTime);
+
+            // Rotate the player to face the correct direction.
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            // Move the player.
+            Vector3 moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
     }
 }
