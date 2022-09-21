8fbc98ae5ee2a22f7f2c03491e880d43af193ca6
Christian-Placencia <chbasaldua123@gmail.com>
2022-09-21 13:41:12 -0500

    Routine Inspection

---
+++ vortex-brujula/Assets/Scripts/Controllers/AIController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AIController", menuName = "InputController/AIController")]
+
+public class AIController : InputController
+{
+    public override bool RetrieveJumpInput()
+    {
+        throw true;
+    }
+
+
+    public override float RetrieveMoveInput();
+    {
+        return 1f;
+    }
+
+
+}
